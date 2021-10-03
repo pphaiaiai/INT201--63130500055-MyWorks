0c2e3e3d32e651c1992706d2f5eed0df6a73211f
pphaiaiai <tarathep.siripis@mail.kmutt.ac.th>
2021-10-03 17:31:28 +0700

    update week7

---
+++ week7/script.js
@@ -0,0 +1,52 @@
+let products = [
+  { id: 1, price: 10 },
+  { id: 2, price: 5 },
+];
+
+let newProduct = { id: 3, price: 20 };
+console.log(products);
+products = [...products, { id: newProduct.id, price: newProduct.price }];
+
+//destructuring +rest parameter
+function getStudentsInClass(instructor, ta, ...students) {
+  return students;
+}
+
+let studentSec2 = (getStuden[(instructor, ta, { ...student })] = [
+  "Umaporn",
+  "Tisanai",
+  "A",
+  "B",
+  "C",
+]);
+console.log(student);
+
+//1.
+const myFunc = function (instructor, ta, ...students) {
+  let greeting = "Hello";
+  return students;
+};
+
+const myFunc = (instructor, ta, ...students) => student;
+
+
+//2.
+function echo(text) {
+  return text;
+}
+
+const echo 
+
+
+//3.
+function sayHi(greeting, name) {
+  return `${greeting}, ${name}`;
+}
+
+
+
+//4.
+function getClass() {
+  return `INT201`;
+}
+
